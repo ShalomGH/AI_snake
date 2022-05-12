0ae101807f9715df57cdf183153d4e40938ffe78
ShalomGH <ShalomGH@mail.ru>
2022-05-12 13:57:27 +0600

    normal snake

--- main.py
+++ main.py
@@ -1,60 +1,26 @@
-import pygame
-import random
+from snake import Snake
+from game import Game
+from food import Food
 
-WIDTH = 720  # ширина игрового окна
-HEIGHT = 480 # высота игрового окна
-FPS = 30 # частота кадров в секунду
+game = Game()
+snake = Snake(game.green)
+food = Food(game.brown, game.screen_width, game.screen_height)
 
-# Задаем цвета
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+game.init_and_check_for_errors()
+game.set_surface_and_title()
 
+while True:
+    snake.change_to = game.event_loop(snake.change_to)
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 10))
-        self.image.fill(GREEN)
-        self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+    snake.validate_direction_and_change()
+    snake.change_head_position()
+    game.score, food.food_pos = snake.snake_body_mechanism(game.score, food.food_pos, game.screen_width, game.screen_height)
+    snake.draw_snake(game.play_surface, game.white)
 
-    def update(self):
-        self.rect.x += 10
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
+    food.draw_food(game.play_surface)
 
+    snake.check_for_boundaries(
+        game.game_over, game.screen_width, game.screen_height)
 
-# Создаем игру и окно
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
-clock = pygame.time.Clock()
-all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
-
-# Цикл игры
-running = True
-while running:
-    # Держим цикл на правильной скорости
-    clock.tick(FPS)
-    # Ввод процесса (события)
-    for event in pygame.event.get():
-        # check for closing window
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Обновление
-    all_sprites.update()
-
-    # Рендеринг
-    screen.fill(BLACK)
-    all_sprites.draw(screen)
-    # После отрисовки всего, переворачиваем экран
-    pygame.display.flip()
-
-pygame.quit()
+    game.show_score()
+    game.refresh_screen()
